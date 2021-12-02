9dc874a0382e16b938d9cbac50952d4441299581
Ubuntu <ubuntu@ip-172-31-35-9.ap-northeast-2.compute.internal>
2021-12-02 06:14:43 +0000

    restore media

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%ff7hw_z%_bybjj$_ne1-8%cvgj4d!=2wj8n=w@u065b*m3rrf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ["*",]
 
